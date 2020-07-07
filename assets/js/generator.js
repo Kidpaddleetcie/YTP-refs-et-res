854df62a6578748723419adc252a4eac4b5f874a
KidpaddleetcieGlin <37848836+Kidpaddleetcie@users.noreply.github.com>
2020-07-07 23:00:37 +0200

    remise en question du design

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -1,9 +1,6 @@
-// <div class="g-ytsubscribe" data-channelid="UC_NukSq2ve_AHVAz_9ikTDg" data-layout="full" data-theme="dark" data-count="default"></div>
 // <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/W_Xd8n01bNU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-// Accumulateur des auteurs
-let a = 0;
-let a_authors = ' ';
+
 
 // Les accumulateurs des types
 let i = 0;
@@ -11,65 +8,72 @@ let ii = 0;
 let iii = 0;
 
 let i_sourc = ' ';
+let les_sources = ' ';
 let ii_fanar = ' ';
 let iii_musi = ' ';
 
-function remiseAzero() {
-    i_sourc = " ";
-    ii_fanar = " ";
-    iii_musi = " ";
-    oe.innerHTML = " ";
-}
 
-// Affichage des auteurs
-function auth() {
-    for (a = 0; a < autors.length; a++) {
-        if (autors[a].plateform === plateform[0]) {
-            a_authors += '<div class="g-ytsubscribe" data-channelid="' + autors[a].id + '" data-layout="full" data-theme="dark" data-count="default"></div>';
-        }
-    }
-    as.innerHTML = a_authors;
-}
 
-// Les boutons et construction
+/* Les boutons et construction */
+/* bouton source */
+
 function sour() {
-    ti.innerHTML = "Les sources";
-    auth()
-    remiseAzero()
-    document.title = "Les sources";
-    for (i = 0; i < sourc.length; i++) {
-        i_sourc += '<div class="embed-responsive embed-responsive-16by9"><iframe  class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/' + sourc[i] + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+    document.getElementById("main").style.display = "none";
+
+    i_sourc = '<div class="h-50 d-inline-block" ><iframe class="embed-responsive-item h-50 d-inline-block" src="https://www.youtube-nocookie.com/embed/' + sourc[i] + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+    i_sourc += '<div class="gridou">';
+    if (i != 0) {
+        i_sourc += '<button id="pre_" type="button" class="btn btn-outline-dark"> <i class="fas fa-arrow-left"></i>Précédent </button>';
+    } else {
+        i_sourc += '<button id="pre_" type="button" class="btn" disabled"> Précédent </button>';
     }
-    oe.innerHTML = i_sourc;
+    if (i != sourc.length) {
+        i_sourc += '<button id="sui_" type="button" class="btn btn-outline-dark"> Suite <i class="fas fa-arrow-right"></i> </button>';
+    } else {
+        i_sourc += '<button id="sui_" type="button" class="btn " disabled> Suite </button>';
+    }
+    i_sourc += '</div>';
+    so.innerHTML = i_sourc;
+    document.getElementById("pre_").addEventListener("click", pre_source);
+    document.getElementById("sui_").addEventListener("click", sui_source);
+
+    for (let lsi = 0; lsi < sourc.length; lsi++) {
+        les_sources += '<option value="' + lsi + '">https://www.youtube.com/watch?v=' + sourc[lsi] + '</option>';
+    }
+    sos.innerHTML = les_sources;
 }
+document.getElementById("main").addEventListener("click", sour);
+// précédent
+function pre_source() {
+    if (i != 0) {
+        i--;
+        sour();
+    }
+}
+
+// suite
+function sui_source() {
+    if (i != sourc.length) {
+        i++;
+        sour();
+    }
+}
+
+/* bouton fan-art */
 
 function fana() {
-    auth()
-    remiseAzero()
-    ti.innerHTML = "Les fan-arts";
-    document.title = "Les fan-arts";
     for (ii = 0; ii < fanar.length; ii++) {
         if (fanar[ii].plateform === plateform[0]) {
             ii_fanar += '<div class="embed-responsive embed-responsive-4by3"><iframe  class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/' + fanar[i].link + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
         }
     }
-    oe.innerHTML = ii_fanar;
 }
+/* bouton musique */
 
 function musi() {
-    auth()
-    remiseAzero()
-    ti.innerHTML = "Les musiques";
-    document.title = "Les musiques";
     for (iii = 0; iii < musiq.length; iii++) {
         if (musiq[iii].plateform === plateform[0]) {
             iii_musi += '<div class="embed-responsive embed-responsive-4by3"><iframe  class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/' + musiq[i].link + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
         }
     }
-    oe.innerHTML = iii_musi;
 }
-
-/* Les boutons */
-so.addEventListener("click", sour);
-fa.addEventListener("click", fana);
-mu.addEventListener("click", musi);
--- assets/js/lists.js
+++ assets/js/lists.js
@@ -1,52 +1,16 @@
 /*Les zones*/
-const so = document.getElementById("sou");
-const fa = document.getElementById("fan");
-const mu = document.getElementById("mus");
-const as = document.getElementById("authors");
-const oe = document.getElementById("oeuvres");
-const ti = document.getElementById("titre");
-
-// Les genres de plateformes
-const plateform = ["youtube", "twitter", "soundcloud", "tumblr", "deviantart", "instagram"]
+const so = document.getElementById("_sou");
+const sos = document.getElementById("sousou");
 
 
-/* Les créateurs */
-const autors = [{
-        name: "Layton's World",
-        plateform: plateform[0],
-        id: "UCKz3LAuAOgdD47bwPU4vV0A"
-    },
-    {
-        name: "Olive et Tom | Officiel",
-        plateform: plateform[0],
-        id: "UCWQ2PIaUUzmXG8VWwdKCHwA"
-    },
-    {
-        name: "RosaireTV",
-        plateform: plateform[0],
-        id: "UCWr_C9DsX_Rx_3g_IxTaQ1g"
-    },
-    {
-        name: "Yoshipuff Félix",
-        plateform: plateform[0],
-        id: "UCG1h6IDf9PSYc2tfQG-prdg"
-    },
-    {
-        name: "Bonne Nuit Les Petits - Chaine Officielle",
-        plateform: plateform[0],
-        id: "UCX6DywxDwQ8tyQkSnqgbRZg"
-    },
-    {
-        name: "Futureidiots",
-        plateform: plateform[0],
-        id: "UCJSd4lWard3TBg2OmaMlLfg"
-    },
-    {
-        name: "Dominic Ninmark",
-        plateform: plateform[0],
-        id: "UCBUc9zdtUNTvXU1XRJosj5A"
-    }
-];
+
+const fa = document.getElementById("_fan");
+const mu = document.getElementById("_mus");
+
+
+// Les plateformes
+const plateform = ["youtube", "deviantart", "tumblr"];
+
 /* Les sources */
 const sourc = [
     "zwUzO29aqhY#=_=",
