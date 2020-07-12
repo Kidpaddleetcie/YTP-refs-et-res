0356082db313c00d7e8b89b55b3fcbc1c11c7b7a
KidpaddleetcieGlin <37848836+Kidpaddleetcie@users.noreply.github.com>
2020-07-12 02:49:09 +0200

    Changement de design et amélioration du rendu

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -15,49 +15,10 @@ let iii_musi = ' ';
 
 
 /* Les boutons et construction */
-/* bouton source */
 
-function sour() {
-    document.getElementById("main").style.display = "none";
 
-    i_sourc = '<div class="h-50 d-inline-block" ><iframe class="embed-responsive-item h-50 d-inline-block" src="https://www.youtube-nocookie.com/embed/' + sourc[i] + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
-    i_sourc += '<div class="gridou">';
-    if (i != 0) {
-        i_sourc += '<button id="pre_" type="button" class="btn btn-outline-dark"> <i class="fas fa-arrow-left"></i>Précédent </button>';
-    } else {
-        i_sourc += '<button id="pre_" type="button" class="btn" disabled"> Précédent </button>';
-    }
-    if (i != sourc.length) {
-        i_sourc += '<button id="sui_" type="button" class="btn btn-outline-dark"> Suite <i class="fas fa-arrow-right"></i> </button>';
-    } else {
-        i_sourc += '<button id="sui_" type="button" class="btn " disabled> Suite </button>';
-    }
-    i_sourc += '</div>';
-    so.innerHTML = i_sourc;
-    document.getElementById("pre_").addEventListener("click", pre_source);
-    document.getElementById("sui_").addEventListener("click", sui_source);
 
-    for (let lsi = 0; lsi < sourc.length; lsi++) {
-        les_sources += '<option value="' + lsi + '">https://www.youtube.com/watch?v=' + sourc[lsi] + '</option>';
-    }
-    sos.innerHTML = les_sources;
-}
-document.getElementById("main").addEventListener("click", sour);
-// précédent
-function pre_source() {
-    if (i != 0) {
-        i--;
-        sour();
-    }
-}
 
-// suite
-function sui_source() {
-    if (i != sourc.length) {
-        i++;
-        sour();
-    }
-}
 
 /* bouton fan-art */
 
